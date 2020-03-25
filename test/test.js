40d2232bed373add7eabacd9a72bbf2f531ecbdf
ramazanaslan82 <ramazan_aslan@yahoo.com>
2020-03-25 12:29:41 +0300

    test experiment

--- test/test.js
+++ test/test.js
@@ -6,3 +6,15 @@ describe('Array', function() {
     });
   });
 });
+
+describe('User', function() {
+  describe('#save()', function() {
+    it('should save without error', function(done) {
+      var user = new User('Luna');
+      user.save(function(err) {
+        if (err) done(err);
+        else done();
+      });
+    });
+  });
+});
